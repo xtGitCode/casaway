824c3315c0621b1d177f60482c4cecd525ef6830
xiaothung-infrapro <103571608+xtGitCode@users.noreply.github.com>
2025-09-24 17:33:33 +0800

    loading cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import LoadingCards from "@/components/card/LoadingCards";
 import CategoriesList from "@/components/home/CategoriesList";
 import PropertiesContainer from "@/components/home/PropertiesContainer";
+import { Suspense } from 'react';
 
 function HomePage({searchParams}:{searchParams:{category?:string; search?:string;}}) {
   console.log(searchParams);
@@ -9,10 +11,12 @@ function HomePage({searchParams}:{searchParams:{category?:string; search?:string
         category={searchParams.category}
         search={searchParams.search}
       />
-      <PropertiesContainer
-        category={searchParams.category}
-        search={searchParams.search}
-      />
+      <Suspense fallback={<LoadingCards/>}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
     </section>
   )
 }
