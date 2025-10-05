7568b77d60c2f873ebc49024beaed1868a1ae80f
xiaothung-infrapro <103571608+xtGitCode@users.noreply.github.com>
2025-10-05 16:44:50 +0800

    property components building

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,9 +3,23 @@ import PropertyRating from "@/components/card/PropertyRating";
 import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
+import UserInfo from "@/components/properties/UserInfo";
+import { Separator } from "@/components/ui/separator";
 import { fetchPropertyDetails } from "@/utils/actions";
+import Description from "@/components/properties/Description";
 import { redirect } from "next/navigation";
+import Amenities from "@/components/properties/Amenities";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const DynamicMap = dynamic(
+  () => import('@/components/properties/PropertyMap'), 
+  {
+  ssr: false,
+  loading: () => <Skeleton className="h-[400px] w-full" />,
+});
 
 async function PropertyDetailPage({params}:{params:{id:string}}) {
   const property = await fetchPropertyDetails(params.id);
@@ -13,6 +27,8 @@ async function PropertyDetailPage({params}:{params:{id:string}}) {
   if (!property) redirect('/');
   const {baths, bedrooms, beds, guests} = property
   const details = {baths, bedrooms, beds, guests};
+  const firstName = property.profile.firstName;
+  const profileImage = property.profile.profileImage;
   
   return(
     <section>
@@ -34,6 +50,12 @@ async function PropertyDetailPage({params}:{params:{id:string}}) {
             <h1 className='text-xl font-bold'>{property.name}</h1>
             <PropertyRating inPage propertyId={property.id} />
           </div>
+          <PropertyDetails details={details} />
+          <UserInfo profile={{profileImage, firstName}} />
+          <Separator className="mt-4" />
+          <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           {/* calandar*/}
