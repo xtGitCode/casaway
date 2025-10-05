6cbeae3702f5e95cdd72d94f71970f87e576515a
xiaothung-infrapro <103571608+xtGitCode@users.noreply.github.com>
2025-10-05 17:07:48 +0800

    Update PropertyCard.tsx

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -27,7 +27,7 @@ function PropertyCard({ property }: { property: PropertyCardProps }) {
             {name.substring(0, 30)}
           </h3>
           {/* property rating */}
-          <PropertyRating propertyId={propertyId} inPage={false} />
+          <PropertyRating inPage={false} />
         </div>
         <p className='text-sm mt-1 text-muted-foreground '>
           {tagline.substring(0, 40)}
